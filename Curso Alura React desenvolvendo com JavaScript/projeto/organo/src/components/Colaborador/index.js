943e2a4a150b6eaed2dc465f62d25e2e3ef989e0
Giovanni <giovannirprovazi@gmail.com>
2025-05-09 17:50:36 -0300

    Finalizando criacao do componente time e iniciando criacao do componente colaborador

---
+++ Curso Alura React desenvolvendo com JavaScript/projeto/organo/src/components/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/giprovazi.png' alt='Giovanni Romano'/>
+            </div>
+            <div className='rodape'>
+                <h4>Giovanni Romano</h4>
+                <h5>Devops</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
